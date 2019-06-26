f65ae630d0ecc0a037ab6c64e24cc8a5f09a58fc
Colin Geukes <colingeukes@gmail.com>
2019-06-26 21:52:29 +0200

    Updates intro screen, now content can be added

---
+++ website/public/js/index.js
@@ -0,0 +1,28 @@
+const LOGO_NAV_TRANSITION = '1.5s';
+
+$(document).ready(function () {
+	const landingPage = $('#landing-page');
+	const content = $('#content');
+	const nav = $('nav');
+
+	// Clicking on the welcome page will.
+	landingPage.click(function () {
+		// console.log('clicked')
+		landingPage.css('transition', LOGO_NAV_TRANSITION);
+		landingPage.css('height', '157px');
+		landingPage.css('cursor', 'default');
+		$('#landing-page .container').css('margin', '0 auto');
+		$('#bubble-welcome').hide();
+		content.css('transition', LOGO_NAV_TRANSITION);
+		content.css('margin-top', '85px');
+		nav.css('transition', LOGO_NAV_TRANSITION);
+		nav.css('top', '0');
+
+		setTimeout(function () {
+			$('nav .nav-icon').css('opacity', '1');
+			landingPage.css('transition', '0s');
+			nav.css('transition', '0s');
+			content.css('transition', '0s');
+		}, 850)
+	});
+});
