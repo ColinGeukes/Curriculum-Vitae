61707ad2b59d32ef3d2c9894a87b6beac7c3e9ff
Colin Geukes <colingeukes@gmail.com>
2019-07-03 21:18:44 +0200

    Adds testing of website

---
+++ website/test/client/skillTest.js
@@ -0,0 +1,80 @@
+`use strict`;
+
+const chai = require('chai');
+const {expect} = chai;
+const rewire = require('rewire');
+// The skill module
+const skillModule = rewire('../../public/js/skill.js');
+const Skill = skillModule.__get__('Skill');
+// The skills
+let extra;
+let name;
+let skill;
+let stars;
+
+describe('Skill', () => {
+	context('constructor', () => {
+		beforeEach((done) => {
+			name = 'Java';
+			stars = 5;
+			extra = '11+ years';
+			skill = new Skill(name, stars, extra);
+			done();
+		});
+
+		it('Should have correct name', (done) => {
+			expect(skill.name).to.be.equal(name);
+			done();
+		});
+
+		it('Should have correct amount of stars', (done) => {
+			expect(skill.stars).to.be.equal(stars);
+			done();
+		});
+
+		it('Should have correct extra field', (done) => {
+			expect(skill.extra).to.be.equal(extra);
+			done();
+		});
+	});
+
+	context('comparator', () => {
+		it('Sorting based on stars', (done) => {
+			let skill1 = new Skill('a', 5);
+			let skill2 = new Skill('b', 3);
+			let skill3 = new Skill('c', 4);
+			let array = [skill1, skill2, skill3].sort(Skill.compare);
+
+			// Check equalities.
+			expect(array[0]).to.be.equal(skill1);
+			expect(array[1]).to.be.equal(skill3);
+			expect(array[2]).to.be.equal(skill2);
+			done();
+		});
+
+		it('Sorting based on name', (done) => {
+			let skill1 = new Skill('c', 3);
+			let skill2 = new Skill('a', 3);
+			let skill3 = new Skill('b', 3);
+			let array = [skill1, skill2, skill3].sort(Skill.compare);
+
+			// Check equalities.
+			expect(array[0]).to.be.equal(skill2);
+			expect(array[1]).to.be.equal(skill3);
+			expect(array[2]).to.be.equal(skill1);
+			done();
+		});
+
+		it('Sorting based on equality, no change', (done) => {
+			let skill1 = new Skill('a', 3, 'extra_1');
+			let skill2 = new Skill('a', 3, 'extra_2');
+			let array = [skill1, skill2].sort(Skill.compare);
+
+			// Check equalities.
+			expect(array[0].extra).to.be.equal('extra_1');
+			expect(array[1].extra).to.be.equal('extra_2');
+			done();
+		});
+	});
+});
+
